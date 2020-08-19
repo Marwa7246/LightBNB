0f8a96ecdd3adfe5eed1a3335235f4e75697d923
Marwa Ragheb <marwa.ragheb@gmail.com>
2020-08-19 21:50:01 +0000

    Add Data to tables

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
-  PASSWPRD VARCHAR(255) NOT NULL
+  PASSWORD VARCHAR(255) NOT NULL
 
 );
 
@@ -21,7 +21,7 @@ CREATE TABLE properties (
   owner_id INTEGER NOT NULL REFERENCES users(id),
   title VARCHAR(255) NOT NULL,
   description TEXT,
-  thumbnal_photo_url VARCHAR(255) NOT NULL,
+  thumbnail_photo_url VARCHAR(255) NOT NULL,
   cover_photo_url VARCHAR(255) NOT NULL,
   cost_per_night INTEGER NOT NULL DEFAULT 0,
   parking_spaces INTEGER NOT NULL DEFAULT 0,
@@ -32,7 +32,7 @@ CREATE TABLE properties (
   street VARCHAR(255) NOT NULL,
   city VARCHAR(255) NOT NULL,
   province VARCHAR(255) NOT NULL,
-  postant_code VARCHAR(255) NOT NULL,
+  postal_code VARCHAR(255) NOT NULL,
   
   active BOOLEAN NOT NULL DEFAULT TRUE 
 
